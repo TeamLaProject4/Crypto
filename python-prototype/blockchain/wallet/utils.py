0783be4e52fa1b64776157232c2c21c240ee2017
Jeroen <jeroen@lammersma.dev>
2021-12-16 23:01:43 +0100

    Python prototype: private key construction from mnemonic

--- python-prototype/blockchain/wallet/utils.py
+++ python-prototype/blockchain/wallet/utils.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Union
 
-from bip_utils import Bip39MnemonicGenerator, Bip39WordsNum, Bip39Languages, Bip39SeedGenerator
+from bip_utils import Bip39MnemonicGenerator, Bip39WordsNum, Bip39Languages, Bip39SeedGenerator, Bip32Secp256k1, Bip39MnemonicValidator
 from bip_utils.utils.mnemonic.mnemonic import Mnemonic
 
 
@@ -11,23 +11,23 @@ WORDS_NUM = Bip39WordsNum.WORDS_NUM_12
 class WalletUtils():
 
     @staticmethod
-    def generate_mnemonic() -> Mnemonic:
+    def generate_mnemonic() -> str:
         mnemonic = Bip39MnemonicGenerator(LANGUAGE).FromWordsNumber(WORDS_NUM)
-        return mnemonic
+        return str(mnemonic)
 
     @staticmethod
-    def generate_mnemonic_from_words(words: List[str]) -> Mnemonic:
-        return Mnemonic(words)
+    def is_valid_mnemonic(mnemonic: str) -> bool:
+        return Bip39MnemonicValidator().IsValid(mnemonic)
 
     @staticmethod
-    def get_mnemonic_words(mnemonic: Mnemonic) -> List[str]:
-        return mnemonic.m_mnemonic_list
-
-    @staticmethod
-    def generate_seed_bytes(mnemonic: Mnemonic) -> bytes:
+    def generate_seed_bytes(mnemonic: str) -> bytes:
         return Bip39SeedGenerator(mnemonic).Generate()
 
     @staticmethod
-    def generate_seed_bytes_string(mnemonic: Mnemonic) -> str:
+    def generate_seed_bytes_string(mnemonic: str) -> str:
         seed_bytes = WalletUtils.generate_seed_bytes(mnemonic)
         return seed_bytes.hex()
+
+    @staticmethod
+    def construct_private_key(seed_bytes: bytes):
+        return Bip32Secp256k1.FromSeed(seed_bytes).PrivateKey().ToExtended()
