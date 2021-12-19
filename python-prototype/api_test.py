243267b895b2ceba7770d2c95bdac00f80d0c9ac
Jeroen <jeroen@lammersma.dev>
2021-12-19 15:48:07 +0100

    Python-prototype: proof-of-stake base with forger lottery

--- python-prototype/api_test.py
+++ python-prototype/api_test.py
@@ -1,7 +1,8 @@
-from blockchain import TxType
-from blockchain.wallet import Wallet
-from blockchain.node.encoding import Encoding
 import requests
+
+from blockchain import TxType
+from blockchain.node.encoding import Encoding
+from blockchain.wallet import Wallet
 
 if __name__ == '__main__':
     alice = Wallet()
@@ -10,7 +11,7 @@ if __name__ == '__main__':
 
     exchange_tx = exchange.create_transaction(
         alice.public_key_string(), 50, TxType.EXCHANGE)
-    
+
     url = 'http://localhost:5000/transaction'
     body = {'transaction': Encoding.encode(exchange_tx)}
     request = requests.post(url, json=body)
---
+++ python-prototype/blockchain/proof_of_stake/lot.py
@@ -0,0 +1,15 @@
+from blockchain import BlockchainUtils
+
+
+class Lot():
+
+    def __init__(self, public_key_string: str, iteration: int, previous_block_hash: str) -> None:
+        self.public_key_string = public_key_string
+        self.iteration = iteration
+        self.previous_block_hash = previous_block_hash
+
+    def hash(self) -> str:
+        hash = self.public_key_string + self.previous_block_hash
+        for _ in range(self.iteration):
+            hash = BlockchainUtils.hash(hash).hexdigest()
+        return hash
