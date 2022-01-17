b99082a2ac40d3c00d606753aba634410c53577a
LarsCaspers <l.g.caspers@st.hanze.nl>
2022-01-17 16:23:34 +0100

    Add ability to send messages with a custom boot node

--- crypto-backend/network/ldej.go
+++ crypto-backend/network/ldej.go
@@ -2,45 +2,39 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
-	"fmt"
 	"github.com/libp2p/go-libp2p-core/peer"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"io"
 	"log"
-	mrand "math/rand"
 	"sync"
 
-	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/multiformats/go-multiaddr"
 )
 
-func NewHost(seed int64, port int) (host.Host, error) {
-
-	// If the seed is zero, use real cryptographic randomness. Otherwise, use a
-	// deterministic randomness source to make generated keys stay the same
-	// across multiple runs
-	var r io.Reader
-	if seed == 0 {
-		r = rand.Reader
-	} else {
-		r = mrand.New(mrand.NewSource(seed))
-	}
-
-	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
-	if err != nil {
-		return nil, err
-	}
-
-	addr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
-
-	return libp2p.New(
-		libp2p.ListenAddrs(addr),
-		libp2p.Identity(priv),
-	)
-}
+//func NewHost(seed int64, port int) (host.Host, error) {
+//
+//	// If the seed is zero, use real cryptographic randomness. Otherwise, use a
+//	// deterministic randomness source to make generated keys stay the same
+//	// across multiple runs
+//	var r io.Reader
+//	if seed == 0 {
+//		r = rand.Reader
+//	} else {
+//		r = mrand.New(mrand.NewSource(seed))
+//	}
+//
+//	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	addr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
+//
+//	return libp2p.New(
+//		libp2p.ListenAddrs(addr),
+//		libp2p.Identity(priv),
+//	)
+//}
 
 func NewDHT(ctx context.Context, host host.Host, bootstrapPeers []multiaddr.Multiaddr) (*dht.IpfsDHT, error) {
 	var options []dht.Option
--- crypto-backend/network/main.go
+++ crypto-backend/network/main.go
@@ -3,22 +3,77 @@ package main
 import (
 	"bufio"
 	"context"
+	"crypto/rand"
+	"flag"
 	"fmt"
 	"github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/multiformats/go-multiaddr"
+	"io"
+	mrand "math/rand"
 	"os"
+	"strings"
 	"sync"
 )
 
 var logger = log.Logger("cryptomunt")
 
 const RANDEVOUS_STRING = "cryptomunt-randevous"
+const PROTOCOL_ID = "/cryptomunt/1.0.0"
+
+type addrList []multiaddr.Multiaddr
+
+func (al *addrList) String() string {
+	strs := make([]string, len(*al))
+	for i, addr := range *al {
+		strs[i] = addr.String()
+	}
+	return strings.Join(strs, ",")
+}
+
+func (al *addrList) Set(value string) error {
+	addr, err := multiaddr.NewMultiaddr(value)
+	if err != nil {
+		return err
+	}
+	*al = append(*al, addr)
+	return nil
+}
+
+type Config struct {
+	//Port           int
+	//ProtocolID     string
+	//Rendezvous     string
+	//Seed           int64
+	DiscoveryPeers addrList
+}
+
+func main() {
+	config := Config{}
+	ctx, _ := context.WithCancel(context.Background())
+	initLogger()
+
+	flag.Var(&config.DiscoveryPeers, "peer", "Peer multiaddress for peer discovery")
+	flag.Parse()
+
+	node := initHost(ctx, config.DiscoveryPeers)
+
+	logger.Infof("Host ID: %s", node.ID().Pretty())
+	logger.Infof("Connect to me on:")
+	for _, addr := range node.Addrs() {
+		logger.Infof("  %s/p2p/%s", addr, node.ID().Pretty())
+	}
+
+	//run(node, cancel)
+	select {}
+}
 
 func initLogger() {
 	log.SetAllLoggers(log.LevelWarn)
@@ -114,46 +169,102 @@ func initKDHT(ctx context.Context, host host.Host, bootstrapPeers []multiaddr.Mu
 	return kademliaDHT, nil
 }
 
+func NewHost(seed int64, port int) (host.Host, error) {
+
+	// If the seed is zero, use real cryptographic randomness. Otherwise, use a
+	// deterministic randomness source to make generated keys stay the same
+	// across multiple runs
+	var r io.Reader
+	if seed == 0 {
+		r = rand.Reader
+	} else {
+		r = mrand.New(mrand.NewSource(seed))
+	}
+
+	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+	if err != nil {
+		return nil, err
+	}
+
+	addr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
+
+	return libp2p.New(
+		libp2p.ListenAddrs(addr),
+		libp2p.Identity(priv),
+	)
+}
+
 //create new host
-func initHost() host.Host {
+func initHost(ctx context.Context, bootstrapPeers []multiaddr.Multiaddr) host.Host {
 	//host, err := libp2p.New(libp2p.ListenAddrs([]multiaddr.Multiaddr(config.ListenAddresses)...))
-	host, err := libp2p.New()
-	ctx := context.Background()
+	node, err := libp2p.New()
+	//node, err := NewHost(0, 9784)
 	if err != nil {
 		panic(err)
 	}
+	logger.Info("Host created. We are:", node.ID())
+	logger.Info("address: ", node.Addrs())
 
-	logger.Info("Host created. We are:", host.ID())
-	logger.Info("address: ", host.Addrs())
-
-	// Set a function as stream handler. This function is called when a peer
-	// initiates a connection and starts a stream with this peer.
+	// Set a function as stream handler. This function is called when a peerNode
+	// initiates a connection and starts a stream with this peerNode.
 	//protocol id is a unique string on which hosts can agree how to communicate(protocol)?
-	host.SetStreamHandler("/cryptomunt/1.0.0", handleStream)
+	node.SetStreamHandler(protocol.ID(PROTOCOL_ID), handleStream)
+	//node.SetStreamHandler("/cryptomunt/1.0.0", handleStream)
 
 	//initDHT(host)
 	//init dht
-	kademliaDHT, initDHTErr := initKDHT(ctx, host, nil)
+	kademliaDHT, initDHTErr := initKDHT(ctx, node, bootstrapPeers)
 	if initDHTErr != nil {
 		logger.Error("dht error")
 		return nil
 	}
 
-	// We use a rendezvous point to announce our location.
+	//// We use a rendezvous point to announce our location.
+	//// This is like telling your friends to meet you at the Eiffel Tower.
+	//logger.Info("Announcing ourselves...")
+	////routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
+	//go Discover(ctx, node, kademliaDHT, RANDEVOUS_STRING)
+	////discovery.Advertise(ctx, routingDiscovery, RANDEVOUS_STRING)
+	//logger.Debug("Successfully announced!")
+
+	// We use a rendezvous point "meet me here" to announce our location.
 	// This is like telling your friends to meet you at the Eiffel Tower.
 	logger.Info("Announcing ourselves...")
 	routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
 	discovery.Advertise(ctx, routingDiscovery, RANDEVOUS_STRING)
 	logger.Debug("Successfully announced!")
 
-	return host
-}
-func main() {
-	initLogger()
-	host := initHost()
-	logger.Info("Node successfully started. NodeID:", host.ID())
+	// Now, look for others who have announced
+	// This is like your friend telling you the location to meet you.
+	logger.Debug("Searching for other peers...")
+	peerChan, err := routingDiscovery.FindPeers(ctx, RANDEVOUS_STRING)
+	if err != nil {
+		panic(err)
+	}
 
-	select {}
+	for peerNode := range peerChan {
+		if peerNode.ID == node.ID() {
+			continue
+		}
+		logger.Debug("Found peerNode:", peerNode)
+
+		logger.Debug("Connecting to:", peerNode)
+		stream, err := node.NewStream(ctx, peerNode.ID, protocol.ID(PROTOCOL_ID))
+
+		if err != nil {
+			logger.Warn("Connection failed:", err)
+			continue
+		} else {
+			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+			go writeData(rw)
+			go readData(rw)
+		}
+
+		logger.Info("Connected to:", peerNode)
+	}
+
+	return node
 }
 
 func handleStream(stream network.Stream) {
