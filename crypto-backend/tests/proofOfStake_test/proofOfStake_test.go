6beb9299649f0a7aa64c884c3355c4efbee71c22
Jeroen <jeroen@lammersma.dev>
2022-01-29 22:38:11 +0100

    Unit tests for proofOfStake complete

--- crypto-backend/tests/proofOfStake_test/proofOfStake_test.go
+++ crypto-backend/tests/proofOfStake_test/proofOfStake_test.go
@@ -1,9 +1,20 @@
 package proofofstaketest_test
 
 import (
+	"bytes"
 	"cryptomunt/proofOfStake"
+	"math/rand"
 	"testing"
 )
+
+func generateRandomString(length int) string {
+	var letters bytes.Buffer
+	for i := 0; i <= length; i++ {
+		randomChar := 'a' + rune(rand.Intn(26))
+		letters.WriteString(string(randomChar))
+	}
+	return letters.String()
+}
 
 func TestWhenNewPOSThenAccountNotInStakers(t *testing.T) {
 	pos := proofOfStake.NewProofOfStake()
@@ -72,5 +83,57 @@ func TestWhenForgerPickedThenResultIsDeterministic(t *testing.T) {
 		if got != want {
 			t.Errorf("Expected '%s', but got '%s'", want, got)
 		}
+	}
+}
+
+func TestGivenEqualStakeWhenForgerPickedThenItRepresentsStake(t *testing.T) {
+	account_1 := "barrie"
+	account_2 := "sjonnie"
+	pos := proofOfStake.NewProofOfStake()
+	pos.UpdateStake(account_1, 100)
+	pos.UpdateStake(account_2, 100)
+	wins := map[string]int{
+		account_1: 0,
+		account_2: 0,
+	}
+
+	for i := 0; i < 100; i++ {
+		seed := generateRandomString(16)
+		forger := pos.PickForger(seed)
+		if forger == account_1 || forger == account_2 {
+			wins[forger] += 1
+		}
+	}
+
+	got := 40 < wins[account_1] && wins[account_1] < 60 && 40 < wins[account_2] && wins[account_2] < 60
+	want := true
+	if got != want {
+		t.Errorf("Expected '%v', but got '%v'", want, got)
+	}
+}
+
+func TestGivenUnequalStakeWhenForgerPickedThenItRepresentsStake(t *testing.T) {
+	account_1 := "barrie"
+	account_2 := "sjonnie"
+	pos := proofOfStake.NewProofOfStake()
+	pos.UpdateStake(account_1, 10)
+	pos.UpdateStake(account_2, 90)
+	wins := map[string]int{
+		account_1: 0,
+		account_2: 0,
+	}
+
+	for i := 0; i < 100; i++ {
+		seed := generateRandomString(16)
+		forger := pos.PickForger(seed)
+		if forger == account_1 || forger == account_2 {
+			wins[forger] += 1
+		}
+	}
+
+	got := 0 < wins[account_1] && wins[account_1] < 20 && 80 < wins[account_2] && wins[account_2] < 100
+	want := true
+	if got != want {
+		t.Errorf("Expected '%v', but got '%v'", want, got)
 	}
 }
