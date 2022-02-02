7ffa6153deff5146dfadbc647b3788afe1e8b4d7
MartijnEisses <33952780+MartijnEisses@users.noreply.github.com>
2022-02-02 09:26:45 +0100

    Fixed cryptie and payment page

--- frontend/wallet/src/app/cryptie/cryptie.component.ts
+++ frontend/wallet/src/app/cryptie/cryptie.component.ts
@@ -1,42 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cryptie',
   templateUrl: './cryptie.component.html',
-  styleUrls: ['./cryptie.component.scss']
+  styleUrls: ['./cryptie.component.scss'],
 })
 export class CryptieComponent implements OnInit {
-
-  public userForm:FormGroup; // variable is created of type FormGroup is created
+  public userForm: FormGroup; // variable is created of type FormGroup is created
 
   public CryptieQRcode: string = '';
-  public to_amount: string = ''
-  public accountnumber: string = ''
+  public to_amount: string = '';
+  public accountnumber: string = '';
 
-  constructor(  private fb: FormBuilder,
-                private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.userForm = this.fb.group({
       accountnumber: '',
-      to_amount: ''
+      to_amount: '',
     });
-    this.CryptieQRcode = 'test'
+    this.CryptieQRcode = '_';
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  generateQRcode() {
+    this.to_amount = this.userForm.get('to_amount')?.value;
+    this.accountnumber = this.userForm.get('accountnumber')?.value;
+    this.CryptieQRcode =
+      'payment?to=' + this.accountnumber + '&amount=' + this.to_amount;
+    this.router.navigate(['cryptie'], {
+      queryParams: { amount: this.to_amount, to: this.accountnumber },
+    });
   }
-
-  generateQRcode(){
-      this.to_amount=this.userForm.get('to_amount')?.value;
-      this.accountnumber=this.userForm.get('accountnumber')?.value;
-      this.CryptieQRcode = "payment?to=" + this.accountnumber + "&amount=" + this.to_amount
-       this.router.navigate(["cryptie"], {
-         queryParams: { amount: this.to_amount, to: this.accountnumber }
-       }
-       );
-  }
-
-
 }
